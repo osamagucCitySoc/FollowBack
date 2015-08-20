cc52daa3025e72645d814e41dfa9e521a68ee508
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2015-08-20 12:45:50 +0300

    Initial Commit

---
+++ FollowBack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FollowBack
+//
+//  Created by Osama Rabie on 8/20/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FollowBack/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FollowBack
+//
+//  Created by Osama Rabie on 8/20/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
